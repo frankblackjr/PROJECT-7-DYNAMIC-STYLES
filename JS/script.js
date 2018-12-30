e2e61a2b4ef50587c12c6cf4fc05aad4c4939e8f
frankblackjr <35541653+frankblackjr@users.noreply.github.com>
2018-12-30 13:44:35 -0500

    formatted for readability

--- JS/script.js
+++ JS/script.js
@@ -1,87 +1,88 @@
-function onWindowload() {
-  let x = document.getElementById('header-image').style.height;
-  document.getElementById('header').style.height = x;
+/*jshint esversion: 6*/
+//Console Log function
+
+function logger(words) {
+	console.log(words);
 }
 
-//Console Log function
-function logger(words) {
-  console.log(words);
-}
 //paragraph font changing functions
 function changePara() {
-  let x = document.getElementsByTagName('P');
-  for (let i = 0; i < x.length; i++) {
-    x[i].style.fontFamily = "lato";
-  }
+	let x = document.getElementsByTagName('P');
+	for (let i = 0; i < x.length; i++) {
+		x[i].style.fontFamily = "lato";
+	}
 }
 
 function changeBack() {
-  let x = document.getElementsByTagName('P');
-  for (let i = 0; i < x.length; i++) {
-    x[i].style.fontFamily = "open Sans"
-  }
+	let x = document.getElementsByTagName('P');
+	for (let i = 0; i < x.length; i++) {
+		x[i].style.fontFamily = "open Sans"
+	}
 }
 //Company name font change functions
 function pretty() {
-  let x = document.getElementById('center-company-name').style.fontFamily = "Charm";
+	let x = document.getElementById('center-company-name').style.fontFamily = "Charm";
 }
 
 function unpretty() {
-  let x = document.getElementById('center-company-name').style.fontFamily = "open Sans";
+	let x = document.getElementById('center-company-name').style.fontFamily = "open Sans";
 }
+
 //change float position function
 function move() {
-  let x = document.getElementById('mover');
-  if (x.style.cssFloat === "left") {
-    x.style.cssFloat = "right";
-  } else {
-    x.style.cssFloat = "left";
-  }
-  logger("balls");
+	let x = document.getElementById('mover');
+	if (x.style.cssFloat === "left") {
+		x.style.cssFloat = "right";
+	} else {
+		x.style.cssFloat = "left";
+	}
+	logger("balls");
 }
+
 //Image fade in and out/ change image location functions
+
 function pleaseWork() {
-  let x = document.getElementsByClassName("changer");
-  for (var value of x) {
-    fadeOut(value);
-  }
-  var pleaseWork = setInterval(changer(x), 0);
-  for (var value of x) {
-    fadeIn(value);
-  }
+	let x = document.getElementsByClassName("changer");
+	for (let value of x) {
+		fadeOut(value);
+	}
+	var pleaseWork = setInterval(changer(x), 0);
+	for (let value of x) {
+		fadeIn(value);
+	}
 }
 
 function changer(x) {
-  let temp = x[0].src;
-  for (let i = 0; i < x.length; i++) {
-    if (i < 3) {
-      x[i].src = x[i + 1].src;
-    } else {
-      x[i].src = temp;
-    }
-  }
+	let temp = x[0].src;
+	for (let i = 0; i < x.length; i++) {
+		if (i < 3) {
+			x[i].src = x[i + 1].src;
+		} else {
+			x[i].src = temp;
+		}
+	}
 }
 
 function fadeIn(img) {
-  let dec = 0.1;
-  var inter = setInterval(function() {
-    if (dec >= 1) {
-      clearInterval(inter);
-    }
-    img.style.opacity = dec;
-    dec += 0.1;
-  }, 150);
+	let dec = 0.1;
+	var inter = setInterval(function() {
+		if (dec >= 1) {
+			clearInterval(inter);
+		}
+		img.style.opacity = dec;
+		dec += 0.1;
+	}, 150);
 }
 
 function fadeOut(img) {
-  var dec = 1;
-  var inter = setInterval(function() {
-    if (dec <= 0.1) {
-      clearInterval(inter);
-    }
-    img.style.opacity = dec;
-    dec -= 0.1;
-  }, 0);
+	var dec = 1;
+	var inter = setInterval(function() {
+		if (dec <= 0.1) {
+			clearInterval(inter);
+		}
+		img.style.opacity = dec;
+		dec -= 0.1;
+	}, 0);
 }
 
 //Change image ever 5 seconds
